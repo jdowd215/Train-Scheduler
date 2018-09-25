cf53434eee5536bdd59ad69a8cbca31276a4ebb7
JDowd <jamesddowd@outlook.com>
2018-09-25 15:34:40 -0700

    firebase initialize

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,36 @@
+//Steps to complete
+    //1. Initialize Firebase
+    //2. Create way to input additional train info and append it 
+            //underneath the existing train data
+    //3. Create way to calculate "minutes way" data for all trains
+            //and add to the page
+
+
 // Initialize Firebase
   var config = {
-    apiKey:,
+    //apiKey: "AIzaSyCjf5W87dY2SB30K-gSpZ0PSuzu7d3uSVQ",
     authDomain: "train-scheduler-b44bd.firebaseapp.com",
     databaseURL: "https://train-scheduler-b44bd.firebaseio.com",
     projectId: "train-scheduler-b44bd",
     storageBucket: "",
     messagingSenderId: "1031590339358"
   };
+
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  //button for adding train data
+  $("#add-train-btn").on("click", function(event) {
+        event.preventDefault();
+
+  //grabs user input
+    
+
+
+
+  })
+
+
+
+
