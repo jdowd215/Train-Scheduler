8af60a0026301ed59b378c4a494c58101207bbeb
JDowd <jamesddowd@outlook.com>
2018-09-27 17:55:03 -0700

    fix firebase link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@
   var config = {
     apiKey: "AIzaSyCjf5W87dY2SB30K-gSpZ0PSuzu7d3uSVQ",
     authDomain: "train-scheduler-b44bd.firebaseapp.com",
-    databaseURL: "https://train-scheduler-b44bd.firebaseio.com",
+    databaseURL: "https://train-scheduler-b44bd.firebaseio.com/",
     projectId: "train-scheduler-b44bd",
     storageBucket: "",
     messagingSenderId: "1031590339358"
